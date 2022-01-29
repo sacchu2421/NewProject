74b4da087f9477134352b9b9a9de92aa802b5550
mindtreedec60 <mindtreedec60@minddec313.iiht.tech>
2022-01-29 18:59:03 +0530

    first commit

---
+++ NewProject/src/AdditionProgram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the two numbers to add");
+int a =sc.nextInt();
+int b = sc.nextInt();
+int total = a+b;
+System.out.println("The sum is :"+total);
+	}
+
+}
